b9fccc312439ef555ec72e16a75da8b00d411b29
= <=>
2024-06-27 02:06:34 +0300

    #1 Fitness App

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:fitness_app/view/layouts/fitness_layout.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
+  FlutterNativeSplash.remove();
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: false,
+              ),
+      debugShowCheckedModeBanner: false,
+      home: FitnessScreen(),
+    );
+  }
+}
+
+
+// dart run flutter_native_splash:create --path=splash.yaml
